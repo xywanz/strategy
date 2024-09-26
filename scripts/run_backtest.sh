72bcab1a765a323cbe1a1511dab37b216f90f544
k <kevin.lau.gd@gmail.com>
2024-09-26 16:37:14 +0800

    Update run_backtest.sh

--- scripts/run_backtest.sh
+++ scripts/run_backtest.sh
@@ -4,12 +4,8 @@ cd `dirname $0`
 
 mkdir -p ../data ../log
 
-rm -f ../conf/*autogen.json
 rm -f ../data/**
 rm -f ../log/**
-if [ -f "strategy_startup.py" ]; then
-    python3 manager.py stop_strategy strategy_startup.py
-fi
 
 conf_file="../conf/backtest.yaml"
 if [ $# -gt 0 ]; then
